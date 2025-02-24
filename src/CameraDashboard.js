a80897d9b877d78cbb783903b52c2d5c0e2c9388
Alokrajput977 <alokpaswan706@gmail.com>
2025-02-24 15:40:39 +0530

    s

--- src/CameraDashboard.js
+++ src/CameraDashboard.js
@@ -4,7 +4,7 @@ import "video.js/dist/video-js.css";
 import { useNavigate } from "react-router-dom";
 import "./CameraDashboard.css";
 
-const serverIP = "192.168.0.119:3001";
+const serverIP = "10.40.40.49:3001";
 
 const CameraDashboard = () => {
   const [cameras, setCameras] = useState([]);
